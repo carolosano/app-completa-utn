15c83c8c3cb80462c733a69521c6d78834f7fa3b
Caro <losanocarolina2@gmail.com>
2023-01-02 17:52:45 -0300

    App Completa utn

--- index.js
+++ index.js
@@ -43,9 +43,103 @@ conexion.connect((err)=>{
 })
 
 // Rutas de la Aplicacion
-app.get('/', (req, res)=>{
-    res.send(`Bienvenidos a la App Completa`);
+app.get('/', (req, res) => {
+    res.render('index', {
+        titulo: 'Home'
+    })
 })
+
+app.get('/formulario', (req, res) => {
+    res.render('formulario')
+})
+
+app.get('/productos', (req, res) => {
+    
+    let sql = "SELECT * FROM productos";
+
+    conexion.query(sql, function(err, result){
+        if (err) throw err;
+        //console.log(result);
+        res.render('productos', {
+            titulo: 'Productos',
+            datos: result
+        })
+    })
+})
+
+
+app.get('/contacto', (req, res) =>{
+    res.render('contacto', {
+        titulo: 'Contacto'
+    })
+})
+
+app.post('/formulario', (req, res) =>{    
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+    const descripcion = req.body.descripcion;
+
+    let datos = {
+        nombre: nombre,
+        precio: precio,
+        descripcion: descripcion
+    }
+
+    let sql = "INSERT INTO productos set ?";
+
+    conexion.query(sql, datos, function(err){
+        if (err) throw err;
+            console.log(`1 Registro insertado`);
+            res.render('enviado')
+        })
+})
+
+app.post('/contacto', (req, res) =>{
+    const nombre = req.body.nombre;
+    const email = req.body.email;
+
+    //Creamos una función para enviar Email al cliente
+    async function envioMail(){
+        //Configuramos la cuenta del envío­o
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.EMAILPASSWORD
+            }
+        });
+
+        //Enví­o del mail
+        let info = await transporter.sendMail({
+            from: process.env.EMAIL,
+            to: `${email}`,
+            subject: "Gracias por suscribirte a nuestra App",
+            html:`Muchas gracias por visitar nuestra página <br>
+            Recibirás nuestras promociones a esta dirrección de correo. <br>
+            Buen fin de semana!!`
+        })
+    }
+
+    let datos = {
+        nombre: nombre,
+        email: email
+    }
+
+
+    let sql = "INSERT INTO contactos set ?";
+
+    conexion.query(sql, datos, function(err){
+        if (err) throw err;
+            console.log(`1 Registro insertado`);
+            //Email
+            envioMail().catch(console.error);
+            res.render('enviado')
+        })
+
+})
+
 //Servidor a la escucha de las peticiones
 app.listen(PORT, ()=>{
     console.log(`Servidor tranajando en el Puerto: ${PORT}`);
